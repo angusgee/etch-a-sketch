ad03f57a3429f764c2e0e97647ee369f50d8cacc
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-26 21:49:36 +0100

    feat: add colour trail on hover
    - add array of css variable names
    - generate a random int and use it as the arr index

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const container = document.getElementById("container");
 const numSquares = document.getElementById("num-squares-dropdown").value;
 const squaresPerRow = Math.sqrt(numSquares);
-const btn = document.getElementById("create-grid-btn");
+const createGridBtn = document.getElementById("create-grid-btn");
+const clearGridBtn = document.getElementById("clear-grid-btn");
+const colourNames = [
+    "--clr-light-gray",
+    "--clr-medium-gray",
+    "--clr-dark-gray",
+    "--clr-black",
+    "--clr-purple",
+    "--clr-violet",
+    "--clr-blue",
+    "--clr-purple",
+    "--clr-violet",
+    "--clr-blue",
+];
+
+function changeColour(el) {
+    const randInt = parseInt(Math.random() * 10);
+    el.style.backgroundColor = `var(${colourNames[randInt]})`;
+    // console.log(`colour changed to: var(${colourNames[randInt]})`);
+}
 
 function createSquares(numSquares, squaresPerRow) {
     for (let i = 0; i < numSquares; i++) {
@@ -9,13 +28,14 @@ function createSquares(numSquares, squaresPerRow) {
         square.classList.add("square");
         square.style.minHeight = `calc(${100 / squaresPerRow}% - 5.5px)`;
         square.style.minWidth = `calc(${100 / squaresPerRow}% - 5.5px)`;
+        square.addEventListener("mouseenter", () => {
+            changeColour(square);
+        });
         container.appendChild(square);
     }
 }
 
-createSquares(numSquares, squaresPerRow);
-
-btn.addEventListener("click", () => {
+createGridBtn.addEventListener("click", () => {
     const dropdownValue = document.getElementById("num-squares-dropdown").value;
     const squaresPerRow = Math.sqrt(dropdownValue);
     while (container.lastElementChild) {
@@ -23,3 +43,5 @@ btn.addEventListener("click", () => {
     }
     createSquares(dropdownValue, squaresPerRow);
 });
+
+createSquares(numSquares, squaresPerRow);
