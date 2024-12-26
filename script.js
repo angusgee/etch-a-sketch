9884b57daded9844a6e82c6e2853043cec7dde1b
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-26 15:53:12 +0100

    feat: add logic to create new grid on click
    - on click, we first need to remove the existing grid
    - then we call createSquares again

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const container = document.getElementById("container");
-const numSquares = 16;
+const numSquares = 4;
 const squaresPerRow = Math.sqrt(numSquares);
 const btn = document.getElementById("create-grid-btn");
 
-function createSquares(numSquares) {
+function createSquares(numSquares, squaresPerRow) {
     for (let i = 0; i < numSquares; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.minHeight = `calc(${100 / squaresPerRow}% - 5.5px)`;
+        // console.log("min height calculated to: ", square.style.minHeight);
         square.style.minWidth = `calc(${100 / squaresPerRow}% - 5.5px)`;
+        // console.log("min height calculated to: ", square.style.minWidth);
         container.appendChild(square);
     }
 }
 
-createSquares(numSquares);
+createSquares(numSquares, squaresPerRow);
 
 btn.addEventListener("click", () => {
-    // e.preventDefault();
-    console.log(">> clicked! changing grid");
+    const dropdownValue = document.getElementById("num-squares-dropdown").value;
+    // console.log(">>> value selected: ", dropdownValue);
+    const squaresPerRow = Math.sqrt(dropdownValue);
+    // console.log(">>> squares per row calculated to: ", squaresPerRow);
+    // console.log(">> clicked! changing grid to", dropdownValue);
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+    createSquares(dropdownValue, squaresPerRow);
 });
