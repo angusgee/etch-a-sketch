25d934a866ca692a18e69c1c42ff35667e8d921f
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-26 16:04:55 +0100

    fix: removed commented out code

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ function createSquares(numSquares, squaresPerRow) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.minHeight = `calc(${100 / squaresPerRow}% - 5.5px)`;
-        // console.log("min height calculated to: ", square.style.minHeight);
         square.style.minWidth = `calc(${100 / squaresPerRow}% - 5.5px)`;
-        // console.log("min height calculated to: ", square.style.minWidth);
         container.appendChild(square);
     }
 }
@@ -19,10 +17,7 @@ createSquares(numSquares, squaresPerRow);
 
 btn.addEventListener("click", () => {
     const dropdownValue = document.getElementById("num-squares-dropdown").value;
-    // console.log(">>> value selected: ", dropdownValue);
     const squaresPerRow = Math.sqrt(dropdownValue);
-    // console.log(">>> squares per row calculated to: ", squaresPerRow);
-    // console.log(">> clicked! changing grid to", dropdownValue);
     while (container.lastElementChild) {
         container.removeChild(container.lastElementChild);
     }
